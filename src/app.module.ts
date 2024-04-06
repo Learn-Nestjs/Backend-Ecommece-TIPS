46705380aa4163f1f081b4346de54ff8f161a357
dncuong <dinhcuongnd2001@gmail.com>
2024-04-06 22:17:01 +0700

    feat: initial project

---
+++ src/app.module.ts
@@ -0,0 +1,36 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { LoggerModule } from './logger/logger.module';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { HttpExceptionFilter } from './common/http-exception.filter';
+import { TransformInterceptor } from './common/transform.interceptor';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import { GoogleAuthModule } from './google-auth/google-auth.module';
+
+@Module({
+  imports: [
+    LoggerModule,
+    PrismaModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath:
+        process.env.NODE_ENV == 'production' ? './production.env' : './dev.env',
+    }),
+    GoogleAuthModule,
+  ],
+  controllers: [AppController],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: TransformInterceptor,
+    },
+    AppService,
+  ],
+})
+export class AppModule {}
