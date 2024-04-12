6ed8c3a59bd29456af26cde6544454ed9382dfe1
cuongdnjits <cuongdn@jitsinnovationlabs.com>
2024-04-12 13:26:54 +0700

    feat: generate key pair crypto

--- src/shop-schema/shop-schema.ts
+++ src/shop-schema/shop-schema.ts
@@ -1,21 +1,31 @@
 import { ConflictException, HttpException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { ISignUp } from './interfaces/shop.interface';
-
+import * as crypto from 'crypto'
 @Injectable()
 export class shopSchemaService {
-    constructor(private prismaService: PrismaService){}
+    constructor(private prismaService: PrismaService) { }
 
-    async signUp (data : ISignUp) {
+    async signUp(data: ISignUp) {
         const holderShop = await this.prismaService.shopSchema.findFirst({
             where: {
                 email: data.email
             }
         })
 
-        if(holderShop) throw new ConflictException("Email has existin")
-        const test = await this.prismaService.shopSchema.create({data})        
-        console.log('test :', test);
+        // if (holderShop) throw new ConflictException("Email has existin")
+        const { privateKey, publicKey } = await crypto.generateKeyPairSync('rsa', {
+            modulusLength: 4096,
+            publicKeyEncoding: {
+                type: "pkcs1",
+                format: 'pem'
+            },
+            privateKeyEncoding: {
+                type: "pkcs1",
+                format: 'pem'
+            }
+        })
+        // const test = await this.prismaService.shopSchema.create({data})        
     }
 
 }
